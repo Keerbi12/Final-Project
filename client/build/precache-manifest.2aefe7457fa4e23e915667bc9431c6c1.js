9b0b871f1956274442fd9ded72eae8ad6b6a774b
Keerbi Liu <kirby.liu12@gmail.com>
2021-08-05 21:47:11 +1000

    Successfully connected to mongo db atlas

---
+++ client/build/precache-manifest.2aefe7457fa4e23e915667bc9431c6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3552e23c90059c623a1ddec4a3bb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85bff1afd1268162b72c",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "0d138b8b93d29c05c6b2",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "85bff1afd1268162b72c",
+    "url": "/static/js/2.8986ffd5.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.8986ffd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d138b8b93d29c05c6b2",
+    "url": "/static/js/main.9efb75af.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
