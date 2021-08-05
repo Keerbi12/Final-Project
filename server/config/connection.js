38a004396d89117294ede14a9eb96b311e5245bd
Keerbi Liu <kirby.liu12@gmail.com>
2021-08-05 22:50:18 +1000

    Heroku issue #2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-// process.env.MONGODB_URI || 
-
-mongoose.connect('mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
