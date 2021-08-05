db8eb65f3810255869d6b6240855105a17e62df5
Keerbi Liu <kirby.liu12@gmail.com>
2021-08-05 22:30:26 +1000

    Testing moongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+
+// process.env.MONGODB_URI || 
+
+mongoose.connect('mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
