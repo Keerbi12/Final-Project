5fbc99b8bc8f9836b5a7049075b750565d03dcd7
Keerbi Liu <kirby.liu12@gmail.com>
2021-08-08 00:24:59 +1000

    CSS-IN-JS experimenting.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import Button from './components/Button'
+import styled from 'styled-components'
+const Buttonstyled = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+// import Navbar from './components/Navbar';
+
+/* <Router>
+<>
+  <Navbar />
+  <Switch>
+    <Route exact path='/' component={SearchBooks} />
+    <Route exact path='/saved' component={SavedBooks} />
+    <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+  </Switch>
+</>
+</Router> */
+
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <Buttonstyled>CSS-IN-JS</Buttonstyled>
   );
 }
 
